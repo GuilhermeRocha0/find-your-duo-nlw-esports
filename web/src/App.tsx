68794516538a376a0adc0dbf92e179f3860b7fba
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-09-16 12:16:26 -0300

    feat(web): fetch list games and display games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlassPlus } from 'phosphor-react'
+import { useState, useEffect } from 'react'
 
 import './styles/main.css'
 
@@ -6,7 +6,26 @@ import logoImg from './assets/logo-nlw-esports.svg'
 import GameBanner from './components/GameBanner'
 import CreateAddBanner from './components/CreateAddBanner'
 
+interface Game {
+  id: string
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number
+  }
+}
+
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
+
   return (
     <div className="max-w-[1334px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -16,11 +35,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={5}
-        />
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
 
       <CreateAddBanner />
