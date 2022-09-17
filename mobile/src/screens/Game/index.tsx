43cbb384c9873346e380483a4be367c0a71762ee
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-09-17 01:02:37 -0300

    feat(mobile): add navigation between screens Add enter Game screen with the game title and bannerUrl. Add handle to go back to Home screen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,12 +1,56 @@
+import { Image, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Background } from '../../components/Background'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import { Entypo } from '@expo/vector-icons'
+
+import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { styles } from './styles'
+import { THEME } from '../../theme'
+
+import { GameParams } from '../../@types/navigation'
+
+import { Background } from '../../components/Background'
+import { Heading } from '../../components/Heading'
+import { DuoCard } from '../../components/DuoCard'
 
 export function Game() {
+  const navigation = useNavigation()
+  const route = useRoute()
+  const game = route.params as GameParams
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+          <Image source={logoImg} style={styles.logo} />
+          <View style={styles.right}></View>
+        </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading
+          title={game.title}
+          subtitle="Selecione o game que deseja jogar..."
+        />
+
+        <DuoCard />
+      </SafeAreaView>
     </Background>
   )
 }
