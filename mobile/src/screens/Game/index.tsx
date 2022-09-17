52da06888b8b49dd830a8b7b4eb497361063ec1a
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-09-17 02:47:00 -0300

    feat(mobile): add components and show ads on Game - add component DuoCard - add component DuoInfo - add ads on screen Game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,4 +1,5 @@
-import { Image, TouchableOpacity, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRoute, useNavigation } from '@react-navigation/native'
 import { Entypo } from '@expo/vector-icons'
@@ -12,16 +13,24 @@ import { GameParams } from '../../@types/navigation'
 
 import { Background } from '../../components/Background'
 import { Heading } from '../../components/Heading'
-import { DuoCard } from '../../components/DuoCard'
+import { DuoCard, DuoCardProps } from '../../components/DuoCard'
 
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([])
+
   const navigation = useNavigation()
-  const route = useRoute()
-  const game = route.params as GameParams
+  const router = useRoute()
+  const game = router.params as GameParams
 
   function handleGoBack() {
     navigation.goBack()
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.0.12:3333/games/${game.id}/ads`)
+      .then(response => response.json())
+      .then(data => setDuos(data))
+  }, [])
 
   return (
     <Background>
@@ -49,7 +58,24 @@ export function Game() {
           subtitle="Selecione o game que deseja jogar..."
         />
 
-        <DuoCard />
+        <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[
+            duos.length > 0 ? styles.contentList : styles.emptyListContent
+          ]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncio publicados ainda.
+            </Text>
+          )}
+        />
       </SafeAreaView>
     </Background>
   )
